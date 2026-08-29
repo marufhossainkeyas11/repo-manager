7e634cc1790217417a4d233848b8924ddfbda640
marufhossainkeyas11 <166496971+marufhossainkeyas11@users.noreply.github.com>
2026-08-29 09:39:55 +0600

    Update 0 file(s), delete 67 file(s)

--- public/scripts/utils/dom.js
+++
@@ -1,6 +0,0 @@
-export const $ = (id) => document.getElementById(id);
-
-export function escapeHtml(s) {
-  return String(s).replace(/[&<>"']/g, (c) => ({ "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" }[c]));
-}
-export function escapeAttr(s) { return escapeHtml(s); }
